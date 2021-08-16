bc887fbaa4993e9df41f7ed53cf6c3cdb7664f9f
Crowdbotics <team@crowdbotics.com>
2021-08-16 22:45:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "snowy_snowflake_29863Identifier",
+  url: "https://snowy_snowflake_29863Identifier.botics.co",
+  api: "https://snowy_snowflake_29863Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
